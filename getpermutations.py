a82ad08ad562f14193fa2600e974e70766c7b4b2
NecmetdinTURAN <necmetdin.turan@std.bogazici.edu.tr>
2025-03-17 13:50:41 +0300

    main ideas

--- getpermutations.py
+++ getpermutations.py
@@ -1,8 +1,9 @@
+
 def get_permutations(txt=str):
     listoftxt = []
     right_to_the_before =
     left_to_the_before =
     listoftxt.append(txt[len(-1:)])
-    return get_permutations(txt(:-1))+txt(len(txt)) txt(len(txt))+get_permutations(txt(:-1))
+    return listoftxt[].append(get_permutations(txt(:-1))+txt(len(txt)) txt(len(txt))+get_permutations(txt(:-1)))
 
 
